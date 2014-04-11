d299e902a8fe8c53655f5353182e31058a1e8ad3
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-11 01:00:39 -0700

    Use assert_equal and Vedis#close each test

--- test/vedis.rb
+++ test/vedis.rb
@@ -5,50 +5,59 @@
 assert("Vedis#set, Vedis#get") do
   v = Vedis.new
   v.set "hoge", "fuga"
-  v.get("hoge") == "fuga"
+  assert_equal "fuga", v.get("hoge")
+  v.close
 end
 
 assert("Vedis#set, Vedis#get by fixnum value") do
   v = Vedis.new
   v.set "hoge", 1
-  v.get("hoge") == "1"
+  assert_equal "1", v.get("hoge")
+  v.close
 end
 
 assert("Vedis#set, Vedis#get by sym") do
   v = Vedis.new
   v.set :hoge, "fuga"
-  v.get(:hoge) == "fuga"
+  assert_equal "fuga", v.get(:hoge)
+  v.close
 end
 
 assert("Vedis#[]=, Vedis#[]") do
   v = Vedis.new
   v["hoge"] = "fuga"
-  v["hoge"] == "fuga"
+  assert_equal "fuga", v["hoge"]
+  v.close
 end
 
 assert("Vedis#[]=, Vedis#[] by fixnum value") do
   v = Vedis.new
   v["hoge"] = 1
-  v["hoge"] == "1"
+  assert_equal "1", v["hoge"]
+  v.close
 end
 
 assert("Vedis#[]=, Vedis#[] by sym") do
   v = Vedis.new
   v[:hoge] = "fuga"
-  v[:hoge] == "fuga"
+  assert_equal "fuga", v[:hoge]
+  v.close
 end
 
 assert("Vedis#[] by On-Disk Data Store") do
   v = Vedis.new "/tmp/mruby-vedis-test.db"
   v[:hoge] = "fuga"
-  v[:hoge] == "fuga"
+  assert_equal "fuga", v[:hoge]
+  v.close
 end
 
 assert("Vedis#exec") do
   v = Vedis.new
   r1 = v.exec "MSET username james age 27 mail dude@example.com"
   r2 = v.exec "MGET username age mail"
-  r1 == nil and r2 == ["james", "27", "dude@example.com"]
+  assert_equal nil, r1 
+  assert_equal ["james", "27", "dude@example.com"] , r2 
+  v.close
 end
 
 assert("Vedis#append") do
@@ -58,6 +67,7 @@ assert("Vedis#append") do
   assert_equal("aaa bbb", v["hoge"])
   assert_equal(true, v.append("fuga", "ccc"))
   assert_equal("ccc", v["fuga"])
+  v.close
 end
 
 assert("Vedis#append, by sym") do
@@ -67,6 +77,7 @@ assert("Vedis#append, by sym") do
   assert_equal("aaa bbb", v[:hoge])
   assert_equal(true, v.append(:fuga, "ccc"))
   assert_equal("ccc", v[:fuga])
+  v.close
 end
 
 assert("Vedis#<<") do
@@ -75,6 +86,7 @@ assert("Vedis#<<") do
   assert_equal(true, v << {:fuga => "ccc", :hoge => " bbb"})
   assert_equal("aaa bbb", v["hoge"])
   assert_equal("ccc", v["fuga"])
+  v.close
 end
 
 assert("Vedis#exist?") do
@@ -82,6 +94,7 @@ assert("Vedis#exist?") do
   v["hoge"] = "aaa"
   assert_true(v.exists?("hoge"))
   assert_false(v.exists?("fuga"))
+  v.close
 end
 
 assert("Vedis#exist?, by sym") do
@@ -89,6 +102,7 @@ assert("Vedis#exist?, by sym") do
   v[:hoge] = "aaa"
   assert_true(v.exists?(:hoge))
   assert_false(v.exists?(:fuga))
+  v.close
 end
 
 assert("Vedis#strlen") do
@@ -96,6 +110,7 @@ assert("Vedis#strlen") do
   v["hoge"] = "aaa"
   assert_equal(3, v.strlen("hoge"))
   assert_equal(0, v.strlen("fuga"))
+  v.close
 end
 
 assert("Vedis#strlen, by sym") do
@@ -103,4 +118,5 @@ assert("Vedis#strlen, by sym") do
   v[:hoge] = "aaa"
   assert_equal(3, v.strlen(:hoge))
   assert_equal(0, v.strlen(:fuga))
+  v.close
 end
